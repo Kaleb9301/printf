71f8607a96afc28b2243834cb47e4a395cf31e76
Kaleb Wondwossen <kalebwondwossent@gmail.com>
2023-10-17 18:55:10 +0000

    printf

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include <stdarg.h>
+#include "main.h"
+#include <stddef.h>
+#include <stdio.h>
+
+/**
+ * _printf -prints formated text.
+ * @format: the text to be printed
+ *
+ * Return: the chanater printed
+ */
+int _printf(const char *format, ...)
+{
+	int count = 0, i;
+	va_list data;
+
+	va_start(data, format);
+	if(format == NULL)
+		return (-1);
+	for (i = 0; format[i] != '\0'; )
+	{
+		if(format[i] != ''%)
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+		else if (form[i +1] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i +1])
+			{
+				case 'c':
+					count += _putchar(va_arg(data, int));
+					break;
+				case 's':
+					count += print_string(va_arg(data, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				default:
+					count += _putchar(format[i]);
+					i--;
+					break;					
+			}
+			i += 2;
+		}	
+	}
+	va_end(data);
+	return (count);
+}
